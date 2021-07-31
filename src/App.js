4ff23ae902dc976bfd040546463b900b90516bee
Soner Koçak <sonerkocak@gmail.com>
2021-07-31 22:50:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Deneme from "./Deneme";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Deneme baslik="Başlık 01">
+          <h3>Başlık 3</h3>
+          <p>Paragraf 01</p>
+        </Deneme>
+        <Deneme baslik="Başlık 02" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Deneme.js
@@ -0,0 +1,32 @@
+import {useEffect, useState} from "react";
+
+export default function Deneme({ baslik, children }) {
+    const [sayi, setSayi] = useState(0);
+    const [kare, setKare] = useState(0);
+
+    const [metin, setMetin] = useState('');
+
+    useEffect(() => {
+        setKare(sayi * sayi);
+    }, [sayi]);
+
+    return (
+      <div>
+          Deneme {baslik}
+          <br />
+          Sayı: {sayi}
+          <br />
+          Kare: {kare}
+          <br />
+          Metin: {metin}
+          <br />
+          <button onClick={() => setSayi(sayi + 1)}>Artır</button>
+          <br />
+          <button onClick={() => setSayi(0)}>Reset</button>
+          <br />
+          <input value={metin} onChange={e => setMetin(e.target.value)}/>
+          <br />
+          {children}
+      </div>
+    );
+}
